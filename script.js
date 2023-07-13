c779ea56de6c2c7905f17786dab76f04d41f7b1c
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-07-14 06:34:50 +0800

    Fix indentation

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let myLibrary = [];
 
 // Book constructor
 function Book(title, author, pageCount, status) {
-  this.title = title;
-  this.author = author;
-  this.pageCount = pageCount;
-  this.status = status;
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.status = status;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+    // do stuff here
 }
