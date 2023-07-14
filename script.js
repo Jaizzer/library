23f758c9ce6c07715131da7cce920c85eec80bf4
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-07-15 06:48:44 +0800

    Add action buttons to the cards

--- script.js
+++ script.js
@@ -91,5 +91,28 @@ function createCard(Book) {
         card.appendChild(cardContent);
     }
 
+    // Add action buttons inside card.
+    cardContent = document.createElement("div");
+
+    let editButton = document.createElement("button");
+    editButton.className = "edit";
+    editButton.innerHTML = `<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <title>pencil</title>
+                                <path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" />
+                            </svg>`;
+    cardContent.appendChild(editButton);
+
+
+    let deleteButton = document.createElement("button");
+    deleteButton.className = "delete";
+    deleteButton.innerHTML = `<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <title>trash-can-outline</title>
+                                <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
+                            </svg>`;
+    cardContent.appendChild(deleteButton);
+
+    // Append card content inside the card.
+    card.appendChild(cardContent);
+    
     return card;
 }
