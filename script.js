27412412e861f1a38748a18554037753a6c0136d
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-07-18 05:55:11 +0800

    Rename function

--- script.js
+++ script.js
@@ -136,7 +136,7 @@ function createCard(Book) {
         let bookId = getBookId(this)
 
         // Delete book.
-        enableEditFeature(bookId);
+        enableFormEditFeature(bookId);
     })
 
     cardContent.appendChild(editButton);
@@ -243,7 +243,7 @@ function getBookId(button) {
 
 
 // This function edits a book.
-function enableEditFeature(bookId) {
+function enableFormEditFeature(bookId) {
 
     // Get the corresponding card.
     let card = document.querySelector(`#code-${bookId}`);
