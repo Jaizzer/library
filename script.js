9866b5ceaeb29547c1b29d7744d1f4c43b5cfe48
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2023-07-19 06:32:21 +0800

    Change icon title to suit purpose

--- script.js
+++ script.js
@@ -263,7 +263,7 @@ function createCard(Book) {
     let editButton = document.createElement("button");
     editButton.className = "edit";
     editButton.innerHTML = `<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <title>pencil</title>
+                                <title>Edit Book</title>
                                 <path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" />
                             </svg>`;
 
@@ -283,7 +283,7 @@ function createCard(Book) {
     let deleteButton = document.createElement("button");
     deleteButton.className = "delete";
     deleteButton.innerHTML = `<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                <title>trash-can-outline</title>
+                                <title>Delete Book</title>
                                 <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
                             </svg>`;
     
